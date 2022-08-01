167dc078ce0936f7813cef759cd916e7a964a3a5
Kristina Salnyk <kristina.salnyk@gmail.com>
2022-08-01 23:02:57 +0200

    Added script for first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryRef = document.querySelector('div.gallery');
+
+gallaryRef.innerHTML = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img loading="lazy" class="gallery__image lazyload" data-src="${preview}" alt="${description}" /></a>`
+  )
+  .join(' ');
+
+if ('loading' in HTMLImageElement.prototype) {
+  const loadingLazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  loadingLazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  });
+} else {
+  const lazySizes = require('lazysizes');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -1,65 +1,123 @@
 export const galleryItems = [
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
     description: 'Hokkaido Flower',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     description: 'Aerial Beach View',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
     description: 'Flower Blooms',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
     description: 'Alpine Mountains',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
     description: 'Mountain Lake Sailing',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
     description: 'Alpine Spring Meadows',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    preview: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview: 'https://picsum.photos/id/309/510/340.jpg',
+    original: 'https://picsum.photos/id/309/3872/2592.jpg',
+    description: 'Green Maple Leaf in Sunlight',
+  },
+  {
+    preview: 'https://picsum.photos/id/292/510/340.jpg',
+    original: 'https://picsum.photos/id/292/3852/2556.jpg',
+    description: 'Cooking Ingredients',
+  },
+  {
+    preview: 'https://picsum.photos/id/244/510/340.jpg',
+    original: 'https://picsum.photos/id/244/4288/2848.jpg',
+    description: 'Cormorants on the dock at Dreams Riviera Cancún Resort & Spa.',
+  },
+  {
+    preview: 'https://picsum.photos/id/219/510/340.jpg',
+    original: 'https://picsum.photos/id/219/5184/3456.jpg',
+    description: 'Encounter with a leopard',
+  },
+  {
+    preview: 'https://picsum.photos/id/156/510/340.jpg',
+    original: 'https://picsum.photos/id/156/4400/2656.jpg',
+    description: 'Sand Footprints',
+  },
+  {
+    preview: 'https://picsum.photos/id/111/510/340.jpg',
+    original: 'https://picsum.photos/id/111/4400/2656.jpg?',
+    description: 'Vintage Car Street',
+  },
+  {
+    preview: 'https://picsum.photos/id/1084/510/340.jpg',
+    original: 'https://picsum.photos/id/1084/4579/3271.jpg',
+    description: 'Walrus Haulout',
+  },
+  {
+    preview: 'https://picsum.photos/id/1043/510/340.jpg',
+    original: 'https://picsum.photos/id/1043/5184/3456.jpg',
+    description: 'River Beneath Yosemite Cliffs',
+  },
+  {
+    preview: 'https://picsum.photos/id/0/510/340.jpg',
+    original: 'https://picsum.photos/id/0/5616/3744.jpg',
+    description: 'Macbook Laptop on a Desk',
+  },
+  {
+    preview: 'https://picsum.photos/id/281/510/340.jpg',
+    original: 'https://picsum.photos/id/281/4928/3264.jpg',
+    description: 'Skate Tricks',
+  },
+  {
+    preview: 'https://picsum.photos/id/289/510/340.jpg',
+    original: 'https://picsum.photos/id/289/2800/1508.jpg',
+    description: 'Countryside Barn',
+  },
+  {
+    preview: 'https://picsum.photos/id/312/510/340.jpg',
+    original: 'https://picsum.photos/id/312/3888/2592.jpg',
+    description: 'Sweet Honey in a Jar',
+  },
+  {
+    preview: 'https://picsum.photos/id/319/510/340.jpg',
+    original: 'https://picsum.photos/id/319/5445/2768.jpg',
+    description: 'Denim Jacket Camera Strap',
+  },
+  {
+    preview: 'https://picsum.photos/id/338/510/340.jpg',
+    original: 'https://picsum.photos/id/338/2000/1333.jpg',
+    description: 'Person in a Hoodie',
+  },
+  {
+    preview: 'https://picsum.photos/id/337/510/340.jpg',
+    original: 'https://picsum.photos/id/337/1572/1179.jpg',
+    description: 'Field of Gold, Grain',
   },
 ];
